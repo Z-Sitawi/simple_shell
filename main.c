821830c8cfa3e2d7d08bff8ede1217c8eb394202
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-10-13 18:55:18 +0000

    first prototype task 1

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+
+void display_prompt() {
+    printf("#cisfun$ ");
+    fflush(stdout);
+}
+
+void execute_command(char *command) {
+    pid_t child_pid;
+    int status;
+
+    child_pid = fork();
+
+    if (child_pid == -1) {
+        perror("fork");
+    } else if (child_pid == 0) {
+        /* Child process */
+        char *argv[2];
+        argv[0] = command;
+        argv[1] = NULL;
+
+        if (execve(command, argv, NULL) == -1) {
+            perror("execve");
+            exit(1);
+        }
+    } else {
+        /* Parent process */
+        wait(&status);
+    }
+}
+
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+
+    while (1) {
+        display_prompt();
+
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            printf("\n");
+            break; /* End of file (Ctrl+D) was detected */
+        }
+
+        /* Remove newline character */
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0) {
+            break; /* Exit the shell */
+        }
+
+        execute_command(input);
+    }
+
+    return 0;
+}
